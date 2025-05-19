6b0ee23c6f16d170ce787d6c8fbee8e031ebd914
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-05-19 13:05:22 +0900

    refactor: change to content script

---
+++ content.ts
@@ -0,0 +1,26 @@
+import type { PlasmoCSConfig } from "plasmo"
+
+import { Storage } from "@plasmohq/storage"
+
+import type { Option } from "./types"
+
+export const config: PlasmoCSConfig = {
+  matches: ["<all_urls>"]
+}
+
+const storage = new Storage()
+const options = (await storage.get<Option[]>("options")) ?? []
+
+options
+  .filter(({ url }) => location.href.startsWith(url))
+  .forEach(({ url, selector }) => {
+    const elements = document.querySelectorAll(selector)
+    elements.forEach((element) => {
+      element.setAttribute("autocomplete", "off")
+    })
+    console.log(
+      `Removed autocomplete from ${elements.length} elements matching ${selector} on ${url}`
+    )
+  })
+
+export { }
